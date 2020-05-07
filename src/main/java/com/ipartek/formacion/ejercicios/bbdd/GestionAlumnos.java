bbf72e581d142579dab7ce0c798c123e6c6e280c
Alvaro <javaee@javaee-VirtualBox>
2020-05-07 09:06:13 +0200

    Se arregla error en el menu

--- src/main/java/com/ipartek/formacion/ejercicios/bbdd/GestionAlumnos.java
+++ src/main/java/com/ipartek/formacion/ejercicios/bbdd/GestionAlumnos.java
@@ -7,29 +7,70 @@ import com.ipartek.formacion.modelo.Usuario;
 import com.ipartek.formacion.modelo.UsuarioDAOImpl;
 
 public class GestionAlumnos {
+	static Scanner sc=new Scanner(System.in);
 
 	public static void main(String[] args) {
+		int opcion = 0;
+		
+			
+				do {
+					
+					System.out.println("Introduzca una opcion");
+					menu();
+					System.out.println("despues de pintar el menu");
+					opcion = Integer.valueOf(sc.nextLine());
+					System.out.println("despues de pedri dato");
+					switch (opcion) {
+					case 1:
+						insertUsuario();
+						break;
+					case 2:
+						getByIdUsuario();
+						break;
+					case 3:
+						getAllUsuario();
+						break;
+					case 4:
+						updateUsuario();
+						break;
+					case 5:
+						deleteUsuario();
+						break;
+					case 6:
+						getAllByName();
+						break;
+					default:
+						break;
+					}
 
-		// insertUsuario();
+				} while (opcion != 7);
+			
 
-		// getByIdUsuario();
-		//getAllUsuario();
-		// updateUsuario();
-		// deleteUsuario();
-		getAllByName();
+				sc.close();
+		System.out.println("Agur");
+
+	}
+
+	private static void menu() {
+		System.out.println("1.-insertarUsuario");
+		System.out.println("2.-getByIdUsuario");
+		System.out.println("3.-getAllUsuario");
+		System.out.println("4.-updateUsuario");
+		System.out.println("5.-deleteUsuario");
+		System.out.println("6.-getAllByName");
 
 	}
 
 	private static void getAllByName() {
 		System.out.println("-----------------------------INICIO GET_ALL_BY_NAME USUARIO-----------------------------");
-		ArrayList<Usuario>usuarios=new ArrayList<Usuario>();
-		UsuarioDAOImpl usuarioDAOImpl=UsuarioDAOImpl.getInstance();
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+		UsuarioDAOImpl usuarioDAOImpl = UsuarioDAOImpl.getInstance();
 		// pedir por pantalla nombre a buscar
 		try (Scanner sc = new Scanner(System.in)) {
 			System.out.println("Intro nombre a buscar");
-			String nombreABuscar=sc.nextLine();
-			usuarios=usuarioDAOImpl.getAllByName(nombreABuscar);
-			//pintar registros por pantalla
+			String nombreABuscar = sc.nextLine();
+			usuarios = usuarioDAOImpl.getAllByName(nombreABuscar);
+			// pintar registros por pantalla
 			for (Usuario usuario : usuarios) {
 				System.out.println(usuario);
 			}
@@ -121,7 +162,7 @@ public class GestionAlumnos {
 		System.out.println("-----------------------------INICIO INSERT USUARIO-----------------------------");
 		UsuarioDAOImpl usuarioDAOImpl = UsuarioDAOImpl.getInstance();
 		Usuario usuario = new Usuario();
-		try (Scanner sc = new Scanner(System.in);) {
+		try  {
 			System.out.println("Intro nombre");
 			usuario.setNombre(sc.nextLine());
 			usuarioDAOImpl.insert(usuario);
