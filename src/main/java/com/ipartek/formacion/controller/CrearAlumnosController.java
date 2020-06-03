704c4110ba3d4247f6e4bbb1bd23b4e12c85b52d
javaee <javaee@javaee-VirtualBox>
2020-06-03 09:39:56 +0200

    Se agrega editar

--- src/main/java/com/ipartek/formacion/controller/CrearAlumnosController.java
+++ src/main/java/com/ipartek/formacion/controller/CrearAlumnosController.java
@@ -19,6 +19,7 @@ public class CrearAlumnosController extends HttpServlet {
 	private static final String NOMBRE = "nombre";
 	private static final String MENSAJE = "mensaje";
 	private static final String INSERT_OK = "Se inserto correctamente el alumno en el sistema";
+	private static final String UPDATE_OK = "Se actualizó correctamente el alumno en el sistema";
 	private static final String INSERT_KO = "Hubo un error a la hora de insertar el alumno";
 
 	/**
@@ -27,7 +28,23 @@ public class CrearAlumnosController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doPost(request, response);
+		String id=request.getParameter("id");
+		int idBuscar=0;
+		Usuario usuario=new Usuario();
+		UsuarioDAOImpl dao=UsuarioDAOImpl.getInstance();
+		try {
+			if (!id.isEmpty()&&id!=null) {
+				idBuscar=Integer.parseInt(id);
+				usuario=dao.getById(idBuscar);
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}finally {
+			request.setAttribute("usuario", usuario);
+			request.getRequestDispatcher("crear-alumnos.jsp").forward(request, response);
+		}
+
 
 	}
 
@@ -39,23 +56,47 @@ public class CrearAlumnosController extends HttpServlet {
 			throws ServletException, IOException {
 		// recoger el valor del atributo a insertar en la bbdd
 		String nombre = request.getParameter(NOMBRE);
+		String id = request.getParameter("id");
+		int idBuscar=0;
+		
+		Usuario usuario=new Usuario();
+		usuario.setNombre(nombre);
+		
+		UsuarioDAOImpl dao=UsuarioDAOImpl.getInstance();
+		
 		String mensaje = "";
-		// llamar al dao
-		UsuarioDAOImpl dao = UsuarioDAOImpl.getInstance();
-		Usuario pojo = new Usuario();
-		pojo.setNombre(nombre);
-
 		try {
-			dao.insert(pojo);
-			mensaje = INSERT_OK;
+			if (!id.isEmpty()&&id!=null) {
+				//editamos
+				idBuscar=Integer.parseInt(id);
+				usuario.setId(idBuscar);
+				// llamar al dao
+				dao.update(usuario);
+				mensaje =UPDATE_OK;
+			}else {
+				//creamos
+				// llamar al dao
+				dao.insert(usuario);
+				mensaje = INSERT_OK;
+			}
+			
 		} catch (Exception e) {
 			mensaje = INSERT_KO + e.getMessage();
 			e.printStackTrace();
+		}finally {
+			//redireccionar y pasar el mensaje
+			request.setAttribute(MENSAJE, mensaje);
+			request.setAttribute("alerta", new Alerta(mensaje, "success"));
+			request.getRequestDispatcher("crear-alumnos.jsp").forward(request, response);
 		}
+		
+		
 
-		//redireccionar y pasar el mensaje
-		request.setAttribute(MENSAJE, mensaje);
-		request.getRequestDispatcher("crear-alumnos.jsp").forward(request, response);
+
+	
+
+	
+
 	}
 
 }
