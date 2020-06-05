432131caabb095a1660d16ae1d028118d2d03173
javaee <javaee@javaee-VirtualBox>
2020-06-05 14:13:15 +0200

    Añade rol a usuario. En edicion, comprueba que contraseñas  coincidan.

--- src/main/java/com/ipartek/formacion/controller/CrearAlumnosController.java
+++ src/main/java/com/ipartek/formacion/controller/CrearAlumnosController.java
@@ -1,12 +1,16 @@
 package com.ipartek.formacion.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ipartek.formacion.modelo.Rol;
+import com.ipartek.formacion.modelo.RolDAOImpl;
 import com.ipartek.formacion.modelo.Usuario;
 import com.ipartek.formacion.modelo.UsuarioDAOImpl;
 
@@ -21,6 +25,12 @@ public class CrearAlumnosController extends HttpServlet {
 	private static final String INSERT_OK = "Se inserto correctamente el alumno en el sistema";
 	private static final String UPDATE_OK = "Se actualizó correctamente el alumno en el sistema";
 	private static final String INSERT_KO = "Hubo un error a la hora de insertar el alumno";
+	private static final String CREAR_ALUMNOS_JSP = "crear-alumnos.jsp";
+	private static final String LISTAR_ALUMNOS_JSP = "alumnos.jsp";
+	
+	private ArrayList<Usuario>usuarios;
+	private ArrayList<Rol>roles;
+	private RolDAOImpl rolDao;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
@@ -29,20 +39,49 @@ public class CrearAlumnosController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String id=request.getParameter("id");
+		String view="";
+		
 		int idBuscar=0;
 		Usuario usuario=new Usuario();
+		usuarios=new ArrayList<Usuario>();
+		roles=new ArrayList<Rol>();
+		
 		UsuarioDAOImpl dao=UsuarioDAOImpl.getInstance();
+		rolDao=RolDAOImpl.getInstance();
 		try {
-			if (!id.isEmpty()&&id!=null) {
+			if (id!=null &&!id.isEmpty()&&!"0".equals(id)) {// editamos uno existente
 				idBuscar=Integer.parseInt(id);
+				
 				usuario=dao.getById(idBuscar);
+				request.setAttribute("usuario", usuario);
+				
+				roles=rolDao.getAll();
+				request.setAttribute("roles", roles);
+				
+				view=CREAR_ALUMNOS_JSP;
+				
+			}else if("0".equals(id)) {// creamos uno nuevo
+				
+				roles=rolDao.getAll();
+				
+				request.setAttribute("roles", roles);
+				request.setAttribute("usuario", new Usuario());
+				
+				view=CREAR_ALUMNOS_JSP;
+			}			
+			else {// vamos al listado
+				
+				usuarios=dao.getAll();
+				request.setAttribute("alumnos", usuarios);
+				// setear la redireccion a la vista listado de usuarios
+				view=LISTAR_ALUMNOS_JSP;
 			}
 			
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}finally {
-			request.setAttribute("usuario", usuario);
-			request.getRequestDispatcher("crear-alumnos.jsp").forward(request, response);
+			
+			request.getRequestDispatcher(view).forward(request, response);
 		}
 
 
@@ -54,28 +93,53 @@ public class CrearAlumnosController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// recoger el valor del atributo a insertar en la bbdd
+
+		String mensaje = "";
+		Usuario usuario=new Usuario();
+		try {
+		// 											recoger el valor del atributo a insertar en la bbdd
 		String nombre = request.getParameter(NOMBRE);
 		String id = request.getParameter("id");
-		int idBuscar=0;
+		String pass=request.getParameter("pass");
 		
-		Usuario usuario=new Usuario();
+
+		
+		String idRol=request.getParameter("id_rol");
+		int idBuscar=0;
+		// 											seteamos valores en el pojo usuario
+		
 		usuario.setNombre(nombre);
 		
-		UsuarioDAOImpl dao=UsuarioDAOImpl.getInstance();
 		
-		String mensaje = "";
-		try {
-			if (!id.isEmpty()&&id!=null) {
+		Rol rol=new Rol();
+		rol.setId(Integer.parseInt(idRol));
+		
+		usuario.setRol(rol);
+				
+		UsuarioDAOImpl dao=UsuarioDAOImpl.getInstance();
+		rolDao=RolDAOImpl.getInstance();
+		
+		roles=rolDao.getAll();
+			
+		request.setAttribute("roles", roles);
+		
+		if ("contraseinas no coinciden".equals(pass)) {
+			throw new Exception("contraseinas no coinciden");
+		}			
+		usuario.setContrasenia(pass);	
+		
+			if (!"0".equals(id)) {
 				//editamos
 				idBuscar=Integer.parseInt(id);
 				usuario.setId(idBuscar);
+				
 				// llamar al dao
 				dao.update(usuario);
 				mensaje =UPDATE_OK;
 			}else {
 				//creamos
 				// llamar al dao
+				
 				dao.insert(usuario);
 				mensaje = INSERT_OK;
 			}
@@ -85,6 +149,9 @@ public class CrearAlumnosController extends HttpServlet {
 			e.printStackTrace();
 		}finally {
 			//redireccionar y pasar el mensaje
+			request.setAttribute("roles", roles);
+			
+			request.setAttribute("usuario", usuario);
 			request.setAttribute(MENSAJE, mensaje);
 			request.setAttribute("alerta", new Alerta(mensaje, "success"));
 			request.getRequestDispatcher("crear-alumnos.jsp").forward(request, response);
